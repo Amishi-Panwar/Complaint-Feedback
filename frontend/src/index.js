540907d9e7f9d17f5e6e0d3f90ce64ce9a5d19d2
Amishi-Panwar <40830858+Amishi-Panwar@users.noreply.github.com>
2020-04-07 16:19:14 +0530

    Add files via upload

---
+++ frontend/src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { render } from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from "./components/header";
+import { AcademicsForm } from "./components/Academics_Form";
+import { InfrastructureForm } from "./components/Infrastructure_Forms";
+import { SanitationForm } from "./components/Sanitation_Forms";
+import { MiscellenousForm } from "./components/Miscellenous_Forms";
+import {Feedback} from "./components/Feedback";
+import {MyComplaints} from "./components/MyComplaints";
+import { ActiveComplaints } from "./components/ActiveComplaints";
+import { Faculty } from "./components/Faculty";
+import { Login } from "./components/Login";
+
+class App extends React.Component{
+    render(){
+        return(
+                <div>
+                    <BrowserRouter>
+                        <Route />
+                            <Switch>
+                                <Route exact path="/" component={ActiveComplaints}/>
+                                <Route path="/academics" component={AcademicsForm} />
+                                <Route path="/infrastructure" component={InfrastructureForm} />
+                                <Route path="/miscellenous" component={MiscellenousForm} />
+                                <Route path="/sanitation" component={SanitationForm} />
+                                <Route path="/mycomplaints" component={MyComplaints}/>
+                                <Route path="/feedback" component={Feedback} />
+                                <Route path="/faculty" component={Faculty} />
+                                <Route path="/login" component={Login} />
+                            </Switch>
+                     </BrowserRouter>
+                    <Header />
+                </div>
+                
+        )
+    }
+}
+
+render(<App/> , window.document.getElementById("app"));
+
